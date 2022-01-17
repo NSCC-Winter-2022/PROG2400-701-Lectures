6c08e27ffab149201b1ae8da1006535abb92a6a9
Brian Shewan <w0111036@nscc.ca>
2022-01-17 12:16:02 -0400

    added craptor class

--- dynamic_arrays/craptor.cpp
+++ dynamic_arrays/craptor.cpp
@@ -1,41 +1,83 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int len = 0;
+class Craptor {
 
-ostream& operator<<(ostream& output, int nums[]) {
-    for (int i = 0; i < len; ++i) {
-        cout << nums[i] << ' ';
+private:
+    int *m_nums;
+    int m_capacity;
+    int m_size;
+
+public:
+    Craptor() : m_nums(nullptr), m_capacity(0), m_size(0) {}
+    virtual ~Craptor() {
+        delete[] m_nums;
+    }
+
+    void add(int num) {
+
+        if (m_size == m_capacity) {
+
+            int *temp = m_nums;
+
+            // allocate a block of memory
+            m_nums = new int[m_capacity + 5];
+            m_capacity += 5;
+
+            int i;
+            for(i = 0; i < m_size; i++) {
+                m_nums[i] = temp[i];
+            }
+
+            delete[] temp;
+        }
+
+        m_nums[++m_size] = num;
+    }
+
+    void resize(int size) {
+        int *temp = m_nums;
+        m_nums = new int[size];
+
+        int i;
+        for(i = 0; i < size; i++) {
+            m_nums[i] = temp[i];
+        }
+        m_size = size;
+
+        delete[] temp;
+    }
+
+    friend ostream& operator<<(ostream& output, Craptor& c);
+};
+
+ostream& operator<<(ostream& output, Craptor& c) {
+
+    cout << "capacity: " << c.m_capacity << ") ";
+    for (int i = 0; i < c.m_size; ++i) {
+        cout << c.m_nums[i] << ' ';
     }
     return output;
 }
 
 int main() {
 
-    // starting point: static array
-    int nums[] = {1, 2, 3, 4, 5};
-    len = sizeof(nums) / sizeof(nums[0]);
+    // create a new Craptor
+    Craptor craptor;
+    craptor.add(1);
+    craptor.add(2);
+    craptor.add(3);
+    craptor.add(4);
+    craptor.add(5);
 
-    cout << nums << endl;
+    cout << craptor << endl;
 
-    // add an element to the array
-    int nums2_len = len + 1;
-    int nums2[nums2_len];
+    // resize the Craptor
+    craptor.resize(3);
 
-    // copy from old array into the new array
-    int i = 0;
-    for (; i < len; ++i) {
-        nums2[i] = nums[i];
-    }
-
-    // add new element to new array
-    nums2[i] = 6;
-
-    // set array len to new array size
-    len = nums2_len;
-
-    cout << nums2 << endl;
+    cout << craptor << endl;
 
     return 0;
 }
