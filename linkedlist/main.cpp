0a11b5bfb6b43d8122aa9bd0eae8f0c4c15846ad
Brian Shewan <w0111036@nscc.ca>
2022-01-21 14:32:19 -0400

    completed linked list example

--- linkedlist/main.cpp
+++ linkedlist/main.cpp
@@ -51,6 +51,43 @@ public:
         }
     }
 
+    void insert(int data, int position) {
+
+        LinkedListNode* new_node = new LinkedListNode();
+        // check to see if memory was allocated
+        new_node->m_data = data;
+
+        // find the node to insert before
+        int curr_pos = 1;
+        LinkedListNode* node = m_start;
+        LinkedListNode* prev = nullptr;
+
+        while (node != nullptr) {
+
+            if (curr_pos++ == position) {
+                break;
+            }
+
+            prev = node;
+            node = node->m_next;
+        }
+
+        if (node != nullptr) {
+            if (prev == nullptr) {
+                // inserting before the first node
+                new_node->m_next = m_start;
+                m_start = new_node;
+            } else {
+                // inserting in the middle
+                new_node->m_next = prev->m_next;
+                prev->m_next = new_node;
+            }
+        } else {
+            cout << "Position doesn't exist." << endl;
+        }
+
+    }
+
     void remove(int data) {
 
         LinkedListNode* node = m_start;
@@ -103,46 +140,70 @@ int main() {
 
     LinkedList list;
 
-    // test 1 - add a node to the list
-    list.add(1);
-
+    // test 1 - add a node to the list
     cout << "Test 1" << endl;
     cout << "------" << endl;
+
+    list.add(1);
 
     cout << list << endl;
 
     // test 2 - add another node to the list
+    cout << "Test 2" << endl;
+    cout << "------" << endl;
+
     list.add(2);
     list.add(3);
     list.add(4);
     list.add(5);
 
-    cout << "Test 2" << endl;
-    cout << "------" << endl;
-
     cout << list << endl;
 
     // test 3 - delete a node in the middle of the chain
-    list.remove(3); // remove the node with the data of 3
-
     cout << "Test 3" << endl;
     cout << "------" << endl;
+
+    list.remove(3); // remove the node with the data of 3
 
     cout << list << endl;
 
     // test 4 - delete the last node in the chain
-    list.remove(5); // remove the node with the data of 5
-
     cout << "Test 4" << endl;
     cout << "------" << endl;
+
+    list.remove(5); // remove the node with the data of 5
 
     cout << list << endl;
 
     // test 5 - delete the first node in the chain
-    list.remove(1); // remove the node with the data of 1
-
     cout << "Test 5" << endl;
     cout << "------" << endl;
+
+    list.remove(1); // remove the node with the data of 1
+
+    cout << list << endl;
+
+    // test 6 - insert a node before a node in the chain
+    cout << "Test 6" << endl;
+    cout << "------" << endl;
+
+    list.insert(6, 2); // insert value 6 before the second node
+
+    cout << list << endl;
+
+    // test 7 - insert a node before an invalid node in the chain
+    cout << "Test 7" << endl;
+    cout << "------" << endl;
+
+    list.insert(10, 4); // insert value 10 before the fourth node
+
+    cout << list << endl;
+
+    // test 8 - insert a node before the first node in the chain
+    cout << "Test 8" << endl;
+    cout << "------" << endl;
+
+    list.insert(7, 1); // insert value 7 before the first node
 
     cout << list << endl;
 
