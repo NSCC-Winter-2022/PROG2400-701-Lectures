cf2fce4aeaaf2e7792901ce35a92d427748bdae3
Brian Shewan <w0111036@nscc.ca>
2022-03-04 13:34:32 -0400

    added remove function to bst

--- binarysearchtree/main.cpp
+++ binarysearchtree/main.cpp
@@ -27,6 +27,67 @@ public:
         insert(num, m_root);
     }
 
+    void remove(int num) {
+
+        NodePtr node = m_root;
+        NodePtr parent = nullptr;
+
+        // find the node to delete
+        while (node != nullptr) {
+            if (num < node->m_data) {
+                parent = node;
+                node = node->m_left;
+            } else if (num > node->m_data) {
+                parent = node;
+                node = node->m_right;
+            } else {
+                // found the node!
+                break;
+            }
+        }
+
+        // did we find the node? exit if didn't
+        if (node == nullptr)
+            return;
+
+        // if a node has two children,
+        // use the right-most node in the left subtree as successor
+        if (node->m_left != nullptr && node->m_right != nullptr) {
+            // start at left subtree
+            NodePtr successor = node->m_left;
+
+            // keep going right as far as possible
+            parent = node;
+            while (successor->m_right != nullptr) {
+                parent = successor;
+                successor = successor->m_right;
+            }
+
+            // swap the data between node and successor
+            node->m_data = successor->m_data;
+            node = successor;
+        }
+
+        // does the node have any children?
+        NodePtr subtree = node->m_left;
+        if (subtree == nullptr) {
+            subtree = node->m_right;
+        }
+
+        // connect children to new parent
+        if (parent == nullptr) {
+            // we are at the root!
+            m_root = subtree;
+        } else if (parent->m_left == node) {
+            parent->m_left = subtree;
+        } else if (parent->m_right == node) {
+            parent->m_right = subtree;
+        }
+
+        // finally delete the node
+        delete node;
+    }
+
 protected:
     void insert(int num, NodePtr& node) {
         if (node == nullptr) {
@@ -74,6 +135,32 @@ int main() {
     bst.insert(4);
     bst.insert(6);
     bst.insert(8);
+
+    cout << bst << endl;
+
+    // test 2 - remove some nodes
+    cout << "Test 2 - remove some nodes" << endl;
+    cout << "--------------------------" << endl;
+
+    bst.remove(2);
+
+    cout << bst << endl;
+
+    cout << "---" << endl;
+
+    bst.remove(3);
+
+    cout << bst << endl;
+
+    cout << "---" << endl;
+
+    bst.remove(7);
+
+    cout << bst << endl;
+
+    cout << "---" << endl;
+
+    bst.remove(5);
 
     cout << bst << endl;
 
